8ee584c50c5b150b83bc2baedc80cdc52c260637
Emile Li Tim Cheong <emile.ltc@gmail.com>
2024-01-12 00:31:38 -0500

    adding assets

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -12,7 +12,7 @@ export class TestimonialsComponent implements OnInit {
     websiteName = '';
     testimonials = [
       {
-        img: "https://drive.google.com/uc?export=view&id=164hpmlt7zY4xz4NdZ9GYPqttEy5GEvBx",
+        img: "assets/images/testimonials/mamta.png",
         header: "super hydrated, sleep peacefully, healthy skin",
         para1: "After only a few days drinking Kangen Water, my family and myself could feel super \
         hydrated and sleep peacefully despite a long tiring day.",
@@ -26,7 +26,7 @@ export class TestimonialsComponent implements OnInit {
         phone: "+230 5252 0710"
       },
       {
-        img: "https://drive.google.com/uc?export=view&id=1QA8qblEgeuvGusxL6nAK5P4v7HClBw8o",
+        img: "assets/images/testimonials/aris.png",
         header: "headache relief, easy to drink 2-3 Liters, bottled water savings",
         para1: "I finally bought my own K8 Kangen machine. I have been getting Kangen water from friends for the last two years. Since the first day, I tried Kangen water, I noticed a significant change in my body and my life.",
         para2: "Here is the main reason that motivated me to buy a K8. I used to have a heavy headache after poor sleep, intense work or any bad events. The first time I tried Kangen, I was preparing to write an exam for a professional certification. Three days later, I realized that I didn't have any headache even if I was sleeping only for 4 to 5 hours. Also, I was easily drinking 2 to 3 liters a day, which was almost impossible. I can feel the benefit of Kangen water.",
@@ -37,7 +37,7 @@ export class TestimonialsComponent implements OnInit {
         phone: "+1-519-868-4375"
       },
       {
-        img: "https://drive.google.com/uc?export=view&id=1Al5Sh_0jpsdoqmOQ30ka5vO5vWPmUlS1",
+        img: "assets/images/testimonials/lynn.jpg",
         header: "improved sleep, better digestion, energy up",
         para1: "Kangen water was truly an eye opener for me, with its good hydration benefits.",
         para2: "Having had a car accident a year ago and having to spend quite a chuck of the year immobile, my body was \
@@ -51,7 +51,7 @@ export class TestimonialsComponent implements OnInit {
         phone: "+254-727-687-013"
       },
       {
-        img: "https://drive.google.com/uc?export=view&id=1U3v068I63eGU_mLJO8gil9Jne_PsC9B9",
+        img: "assets/images/testimonials/diane.png",
         header: "more energy, so shining, less throat pain",
         para1: 'I am Diane Anaba from Douala, Cameroun. I discovered KANGEN water through a good friend. I wanted to do KANGEN experience. The water is just great. I feel good in my body, sleep better, more energy and lighter. My companion said to me that" you are particulary so shining". ',
         para2: "I then sent some Kangen water to my brother-in-law, in Yaoundé. He has cancer of throat. Before he had pain throat and a persistent cough. Now, after drinking Kangen, the pain has lessened and he coughs less. I begin proposing Kangen water now to other sick people.",
@@ -62,7 +62,7 @@ export class TestimonialsComponent implements OnInit {
         phone: "+237-677-645-674"
       },
       {
-        img: "https://drive.google.com/uc?export=view&id=12A3Z3uMBnRPK4Nb5qOfbbiKBWdzRYBmf",
+        img: "assets/images/testimonials/neena.jpg",
         header: "improved blood tests, more immune, bottled water savings",
         para1: "2 years ago, I was overwhelmed with my work and managing my life, no time for sports and how worried I was \
         getting about my health. My general blood tests was not too good. I was feeling tired all the time, \
@@ -80,7 +80,7 @@ export class TestimonialsComponent implements OnInit {
         phone: "+230-5719-1524"
       },
       {
-        img: "https://drive.google.com/uc?export=view&id=1dAIafqwkTjvewa3fIuHC0nJmcGUQz-hl",
+        img: "assets/images/testimonials/jolie.jpg",
         header: "pains disappeared, menstrual remedy, body revitalized",
         para1: "I learned about KANGEN WATER in 2020 in GHANA. As I was suffering from menstruation problems, terrible back pain, I immediately agreed to test Kangen water, for a month.",
         para2: "In less than 2 weeks, things took a different turn, I started to feel positively the KANGEN effects in my body and it was unbelievable - the aches and pains disappeared. In short, I had a new body for the better! My menstrual cycle is back to normal...no back pain or cycle abnormalities. KANGEN WATER had revitalized my body. I recommend this KANGEN WATER to you, for the good health of all your body cells. Today, in CANADA, KANGEN water has become our water for life, with our K8. And I now wholeheartedly hope to help people discover KANGEN water back in Congo, my birthplace!",
